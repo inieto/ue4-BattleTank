8e01fb098796f5a9b6b5ae00d955e1f7c2dc1151
Ignacio Nieto <nietofarias@gmail.com>
2019-08-29 08:16:17 -0300

    Battle Tank - Lecture 187 - Add Tick() to PlayerController()

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -1,6 +1,8 @@
 #include "TankPlayerController.h"
 #include "BattleTank.h"
 
+//Order of things: 1st BeginPlay, 2nd Tick and then all other methods.
+
 void ATankPlayerController::BeginPlay() {
     Super::BeginPlay();
     UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
@@ -13,6 +15,23 @@ void ATankPlayerController::BeginPlay() {
     }
 }
 
+// Called every frame
+void ATankPlayerController::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+    AimTowardsCrosshair();
+    //UE_LOG(LogTemp, Warning, TEXT("Player controller ticking"));
+}
+
 ATank* ATankPlayerController::GetControlledTank() const {
     return Cast<ATank> (GetPawn());
+}
+
+// Start the tank moving the barrel so that a shot would hit where the crosshair intersects the world
+void ATankPlayerController::AimTowardsCrosshair() {  //it's not const since it will change state on the Tank
+    if(!GetControlledTank()) return;
+
+    // Get world location through crosshair (if linetrace)
+    // If it hits the landscape
+        // Tell controlled tank to aim at this point
 }
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -14,6 +14,12 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 
 public:
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;    //virtual allows this method to be overwritten by a child
+    //override says to the compiler that the parent of this class must have the same method we are overriding here
+
     ATank* GetControlledTank() const;
 	void BeginPlay() override;
+private:
+    void AimTowardsCrosshair();
 };
